1665f0d9a4559a24fa49535c38bfd705504ccb00
Roman <kisame2002@mail.ru>
2020-11-12 00:56:31 +0300

    1st

---
+++ VegeStore/VegeStore/Box.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace VegeStore
+{
+    public class Box
+    {
+        private double weight;
+        private double price;
+        public double Weight {
+            get => weight;
+            private set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Неверное значение для веса ящика: {value}");
+                }
+            }
+        }
+        public double Price {
+            get => price;
+            set
+            {
+                if(price > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Неверное значение для цены ящика: {value}");
+                }
+            }
+        }
+        public Box(double weight, double price)
+        {
+            Weight = weight;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Ящик овощей.\nВес: {Weight}кг\nЦена: {Price} рублей/кг";
+        }
+    }
+
+}
---
+++ VegeStore/VegeStore/Container.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VegeStore
+{
+    public class Container
+    {
+        public int Capacity { get; private set; }
+        private List<Box> Boxes = new List<Box>();
+        public Container()
+        {
+            Capacity = new Random().Next(50, 101);
+        }
+    }
+}
